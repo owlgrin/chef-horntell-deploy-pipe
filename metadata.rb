10fcd032807bb1b4e56f0e5fa03b7e4324dc1527
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-01 17:40:54 +0530

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'deploy-pipe'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures deploy-pipe'
+long_description 'Installs/Configures deploy-pipe'
+version          '0.1.0'
+
